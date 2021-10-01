4aff45da20f1dbb6138363d01d9a4a776def3f87
Romulo <romulojusto@gmail.com>
2021-10-01 15:57:37 -0400

    initial commit

---
+++ src/main/java/com/romulo/recipeprojectonspring/RecipeProjectOnSpringApplication.java
@@ -0,0 +1,13 @@
+package com.romulo.recipeprojectonspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeProjectOnSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecipeProjectOnSpringApplication.class, args);
+    }
+
+}
